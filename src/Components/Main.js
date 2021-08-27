a2d684a6ac29eb19e26298c05ac9c4225ab47f4b
Jagarlamudi lakshmi Narayana <jagarlamudilakshman1233@gmail.com>
2021-08-27 08:34:22 +0530

    commit3

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <input type={Text}></input>
+            <button>add</button>
+        </div>
+    )
+}
+
+export default Main
