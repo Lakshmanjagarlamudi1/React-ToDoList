9b9c0f10ca3a111e8444919afff7994127b1f022
Jagarlamudi lakshmi Narayana <jagarlamudilakshman1233@gmail.com>
2021-09-22 16:09:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,2 @@
-import React from 'react'
-import Main from './Components/Main'
-
-const App = () => {
-  return (
-    <div>
-      <h1>Hello </h1>
-      <Main/>
-    </div>
-  )
-}
-
-export default App
+import React,{ useReducer } from "react";
+import
